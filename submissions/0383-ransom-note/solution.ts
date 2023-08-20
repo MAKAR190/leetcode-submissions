4fb0b0f9482a0486f55432f1d298529418a3d82c
makkentoshh <lutskyimakar@gmail.com>
2023-08-20 09:36:49 +0000

    [LeetCode Sync] Runtime - 81 ms (5.04%), Memory - 45.2 MB (100.00%)

---
+++ submissions/0383-ransom-note/solution.ts
@@ -0,0 +1,18 @@
+function canConstruct(ransomNote: string, magazine: string): boolean {
+   const magazineMap = new Map();
+    
+    for (const char of magazine){
+        magazineMap.set(char, (magazineMap.get(char) || 0) + 1);
+    }
+    
+    for(const char of ransomNote){
+       const count = magazineMap.get(char);
+        if (count === undefined || count <= 0) {
+            return false;
+        }
+     
+        magazineMap.set(char, count - 1);
+    }
+    
+    return true;
+};
