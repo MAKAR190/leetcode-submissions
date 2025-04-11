92518507a70cdb202353f03e86cb534962bf1d66
makkentoshh <lutskyimakar@gmail.com>
2025-04-11 18:57:00 +0000

    [LeetCode Sync] Runtime - 2 ms (53.83%), Memory - 23 MB (99.88%)

--- submissions/0344-reverse-string/solution.py
+++ submissions/0344-reverse-string/solution.py
@@ -1,12 +1,14 @@
-class Solution(object):
-    def reverseString(self, s):
-        right = len(s) - 1
-        left = 0
-        while left < right:
-            temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            left += 1;
-            right -= 1;
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left = 0;
+        right = len(s) - 1;
+        
+        while left <= right:
+            s[left], s[right] = s[right], s[left];
             
-        return s;
+            left+=1
+            right-=1
+        
