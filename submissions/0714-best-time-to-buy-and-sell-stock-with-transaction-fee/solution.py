9ac9642ad937e30e9e5172a2f8927393eb16fa4d
makkentoshh <lutskyimakar@gmail.com>
2025-11-23 12:04:23 +0000

    [LeetCode Sync] Runtime - 380 ms (21.78%), Memory - 28.4 MB (37.94%)

---
+++ submissions/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/solution.py
@@ -0,0 +1,35 @@
+# Top-down approach
+# from functools import cache
+# class Solution:
+#    def maxProfit(self, prices: List[int], fee: int) -> int:
+#        n = len(prices)
+#         
+#        @cache
+#         def dp(i, holding):
+#             if i == n:
+#                 return 0
+#            
+#             ans = dp(i + 1, holding)
+#             if holding:
+#                 ans = max(ans, (prices[i] - fee) + dp(i + 1, False))
+#             else: 
+#                 ans = max(ans, (-prices[i]) + dp(i + 1, True))
+#                 
+#             return ans
+#         
+#         return dp(0, False)
+ 
+# Bottom-up approach    
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        dp = [[0 for __ in range(2)] for __ in range (n + 1)]
+        
+        for i in range(n - 1, -1, -1):
+            for j in range(2):
+                if j:
+                    dp[i][j] = max(dp[i + 1][j], (prices[i] - fee) + dp[i + 1][0])
+                else:
+                    dp[i][j] = max(dp[i + 1][j], (-prices[i]) + dp[i + 1][1])
+                    
+        return dp[0][0]
