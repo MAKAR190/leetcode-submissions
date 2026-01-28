3c42236f0a190d3d478d9370bfb9c5d3177370a2
makkentoshh <lutskyimakar@gmail.com>
2026-01-28 14:03:54 +0000

    [LeetCode Sync] Runtime - 4 ms (12.81%), Memory - 19.7 MB (9.82%)

--- submissions/0022-generate-parentheses/solution.py
+++ submissions/0022-generate-parentheses/solution.py
@@ -1,20 +1,34 @@
+# Problem definition
+
+# We need to generate as many as possible valid parentheses given the input variable n
+
+# Approach
+
+# Backtracking
+# Valid parentheses are closed fully (left==right)
+# Store left and right parentheses variable count in each backtracking recusrion frame in order to not generate more parentheses than it is possible and also to check whether the output is valid before backtracking further
+
+# Pseudocode
+
+# Input n=3
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def backtrack(curr, left, right):
-            if len(curr) == 2 * n:
-                ans.append(curr[:])
-                return
-            
-            if left < n:
-                curr += "("
-                backtrack(curr, left + 1, right)
-                curr = curr[:-1]
-            
-            if left > right:
-                curr += ")"
-                backtrack(curr, left, right + 1)
-                curr = curr[:-1]
-        
         ans = []
-        backtrack("", 0, 0)
+
+        def backtrack(left, right, curr):
+            if len(curr) == 2 * n:
+                ans.append("".join(curr[:]))
+                return
+    
+            if left > 0:
+                curr.append("(")
+                backtrack(left - 1, right, curr)
+                curr.pop()
+        
+            if right > left:
+                curr.append(")")
+                backtrack(left, right - 1, curr)
+                curr.pop()
+        
+        backtrack(n, n, []) 
         return ans
