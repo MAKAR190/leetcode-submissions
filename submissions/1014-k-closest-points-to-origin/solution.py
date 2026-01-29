c19b8137a71739d370609dd877dad4516d335767
makkentoshh <lutskyimakar@gmail.com>
2026-01-29 17:27:10 +0000

    [LeetCode Sync] Runtime - 72 ms (35.60%), Memory - 25.3 MB (28.07%)

--- submissions/1014-k-closest-points-to-origin/solution.py
+++ submissions/1014-k-closest-points-to-origin/solution.py
@@ -1,30 +1,13 @@
-# Edge cases and questions
-
-# Approach
-# heap = []
-
-# Iterate through points and calculate the distance between the point and the origin and then push that point straight into the heap (min heap)
-
-# Do it until we reach it for K times (so K closest points)
-# Return the heap
-import math
-import heapq
 class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:        
-        heap = []
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        min_heap = []
         
         for x, y in points:
-            d = -(x**2 + y**2) 
-            if len(heap) < k:
-                heapq.heappush(heap, (d, [x, y]))
-            else:
-                if d > heap[0][0]:
-                    heapq.heappop(heap)
-                    heapq.heappush(heap, (d, [x, y]))
-
+            distance = sqrt(x**2 + y**2)
+            heapq.heappush(min_heap, (-distance, [x, y]))
+            
+            while len(min_heap) > k:
+                heapq.heappop(min_heap)
+        
+        return [heap_element[1] for heap_element in min_heap]
                 
-        
-        return [point for _, point in heap]
-            
-            
-        
