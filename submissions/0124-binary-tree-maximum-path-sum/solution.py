5b7a25d0c6f689e793e6db461457293c85b568af
makkentoshh <lutskyimakar@gmail.com>
2026-02-01 18:03:59 +0000

    [LeetCode Sync] Runtime - 7 ms (78.25%), Memory - 23.9 MB (62.11%)

--- submissions/0124-binary-tree-maximum-path-sum/solution.py
+++ submissions/0124-binary-tree-maximum-path-sum/solution.py
@@ -4,22 +4,49 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+#   1
+# 2   3
+
+# Edge cases
+# 1. Our root can be empty
+
+# Approach
+
+# 1. Start DFS from root
+# 2. For each dfs call, call internal DFS function which returns maximum sum path for current node
+# 3. Store the result for each internal DFS call in the maximum answer variable
+# 4. We handle duplicates by going down on each internal DFS call
+# 5. TC(O(n*n)) SC(O(n))
+
+#               5
+#            4     8
+#          11    13  4
+#        7   2         1
+
+
+# Optimilized from O(n**2) to O(n) by computing the final answer on the fly
+
+
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        ans = float('-inf')
+        if not root:
+            return 0
+
+        ans = root.val
 
         def dfs(node):
             nonlocal ans
-            
+
             if not node:
                 return 0
-    
+
             left = max(dfs(node.left), 0)
             right = max(dfs(node.right), 0)
-            
+
             ans = max(ans, node.val + left + right)
-            return max(left, right) + node.val
+            return node.val + max(left, right)
 
         dfs(root)
         return ans
-
