ca2a6accb5f9594edb8e8993c09d2a793de7c59a
makkentoshh <lutskyimakar@gmail.com>
2026-02-01 11:47:02 +0000

    [LeetCode Sync] Runtime - 915 ms (19.24%), Memory - 71.1 MB (9.95%)

--- submissions/0322-coin-change/solution.py
+++ submissions/0322-coin-change/solution.py
@@ -1,23 +1,26 @@
-# Defining the problem
-# So we need to use infinite number of coins of those which we can access (denominations array) to sum up them to amount and return the fewest possible amount of coins needed. If we cannot do that -- return -1
-
-# TC and SC
-#
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        def dp(a): 
-            if a == 0:
+        if amount == 0:
+            return 0
+
+        def dp(remaining):
+            if remaining == 0:
                 return 0
-            
-            if a < 0: return float('inf')
-            
-            if a in memo:
-                return memo[a]
-            
-            memo[a] = min(dp(a - c) + 1 for c in coins)
-            return memo[a]
-        
+            if remaining < 0:
+                return float('inf')
+            if remaining in memo:
+                return memo[remaining]
+    
+           
+            best = float("inf")
+            for coin in coins:
+                best = min(best, 1 + dp(remaining - coin))
+
+            memo[remaining] = best
+            return best
+    
+
         memo = {}
         ans = dp(amount)
-        return -1 if ans == float('inf') else ans
+        
+        return ans if ans != float('inf') else -1
