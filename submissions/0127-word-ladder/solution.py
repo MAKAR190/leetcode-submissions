3d9ff65395ffc6900d695c07c50f3e1871ec4bae
makkentoshh <lutskyimakar@gmail.com>
2026-03-16 16:34:45 +0000

    [LeetCode Sync] Runtime - 5421 ms (8.62%), Memory - 20.2 MB (93.08%)

--- submissions/0127-word-ladder/solution.py
+++ submissions/0127-word-ladder/solution.py
@@ -1,31 +1,35 @@
-from collections import deque
+from collections import defaultdict, deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        n = len(wordList)
-        words = set(wordList)
-        steps = 1
+        if endWord not in wordList:
+            return 0
+
+        def differs_by_one(str1, str2):
+            differs = 0
+
+            for i in range(len(str1)):
+                if str1[i] != str2[i]:
+                    differs += 1
             
+            return differs == 1
+           
+        wordSet = set(wordList)
         queue = deque([beginWord])
-        visited = set()
-        visited.add(beginWord)
-            
+        seen = set()
+        ans = 1
+
         while queue:
             for _ in range(len(queue)):
-                curr = queue.popleft()
-                if curr == endWord:
-                    return steps
-                    
-                for i in range(len(curr)):
-                    for c in "abcdefghijklmnopqrstuvwxyz":
-                        if c == curr[i]:
-                            continue
-                            
-                        new_curr = curr[:i] + c + curr[i+1:]
-                        if new_curr in words:
-                            queue.append(new_curr)
-                            words.remove(new_curr)    
-                    
-            steps += 1
+                node = queue.popleft()
+
+                if node == endWord:
+                    return ans
             
+                for word in wordSet:
+                    if word not in seen and differs_by_one(word, node):
+                        seen.add(word)
+                        queue.append(word)
+                    
+            ans += 1
+
         return 0
-        
