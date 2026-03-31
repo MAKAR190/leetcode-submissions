e60fd29504052b258092d91359ce2f86d0771c85
makkentoshh <lutskyimakar@gmail.com>
2026-03-31 10:07:46 +0000

    [LeetCode Sync] Runtime - 89 ms (71.77%), Memory - 21 MB (20.25%)

---
+++ submissions/2978-check-if-strings-can-be-made-equal-with-operations-ii/solution.py
@@ -0,0 +1,7 @@
+from collections import defaultdict
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+       return (
+            sorted(s1[::2]) == sorted(s2[::2]) and
+            sorted(s1[1::2]) == sorted(s2[1::2])
+        )
